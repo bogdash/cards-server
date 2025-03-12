fc0ab09ba7214470ef20a96b41443d85244e2967
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-03-12 18:07:42 +0300

    First attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "2.2.3"
 }
 
 group = "org.example"
@@ -10,6 +15,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation(kotlin("test"))
 }
 
